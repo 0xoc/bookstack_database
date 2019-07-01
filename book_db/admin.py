64622140999c98e0192021a3c36f3f1aa95b6609
Ali Parvizi <snparvizi75@gmail.com>
2019-07-01 14:01:39 +0430

    book admin

--- book_db/admin.py
+++ book_db/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
 
-admin.site.register(Publisher)
 
-admin.site.register(Creator)
-admin.site.register(Subject)
-admin.site.register(Book)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+admin.site.register(Publisher, PublisherAdmin)
+
+
+class CreatorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'type']
+
+
+admin.site.register(Creator, CreatorAdmin)
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+admin.site.register(Subject, SubjectAdmin)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'publisher', 'creator_string', 'price', 'isbn']
+    list_filter = ['title', 'publisher']
+
+
+admin.site.register(Book, BookAdmin)
+
