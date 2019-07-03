f01d534ed404e6aa05551003f83ebfe77471473a
Ali Parvizi <snparvizi75@gmail.com>
2019-07-03 13:32:37 +0430

    isbn clean, book volume

--- book_db/models.py
+++ book_db/models.py
@@ -39,6 +39,7 @@ class Book(models.Model):
     issue_date = jmodels.jDateField(blank=True, null=True)
 
     isbn = models.CharField(max_length=255, blank=True, null=True)
+    isbn_clean = models.CharField(max_length=255, blank=True, null=True)
     price = models.PositiveIntegerField(blank=True, null=True)
     image = models.CharField(max_length=255, blank=True, null=True)
     pdf = models.CharField(max_length=255, blank=True, null=True)
@@ -48,6 +49,7 @@ class Book(models.Model):
     doe = models.CharField(max_length=255, blank=True, null=True)
     place = models.CharField(max_length=255, blank=True, null=True)
     edition = models.CharField(max_length=255, blank=True, null=True)
+    volume = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return str(self.title)
